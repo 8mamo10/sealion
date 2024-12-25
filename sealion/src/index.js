fc8aa8ac13765a42ac1f4f874e5fb4cfe1b1f9fb
Hiroki Yamamoto <562105+8mamo10@users.noreply.github.com>
2024-12-25 23:58:32 +0900

    Fix title

--- sealion/src/index.js
+++ sealion/src/index.js
@@ -3,7 +3,7 @@ const html = `<!DOCTYPE html>
   <head>
     <meta charset="UTF-8" />
     <meta name="viewport" content="width=device-width,initial-scale=1" />
-    <title>Todos</title>
+    <title>SeaLion</title>
   </head>
   <body>
     <h1>Todos</h1>
